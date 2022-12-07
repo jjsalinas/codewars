4c9d463c49cdabaf3e168c652ef04a57ab135dea
jjsalinas <jjsalinas@pm.me>
2022-12-07 18:06:51 +0100

    New typescript katas added 2022-12-07:1

--- katas_typescript.ts
+++ katas_typescript.ts
@@ -434,3 +434,28 @@ export function comp(a1: number[] | null, a2: number[] | null): boolean {
 // console.log(comp(a, b)); // expected false
 
 
+/****************************************/
+/*
+https://www.codewars.com/kata/513e08acc600c94f01000001/train/typescript
+
+The rgb function is incomplete. 
+Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. 
+Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+*/
+
+export function rgb(r: number, g: number, b: number): string {
+  const rgbSanitazed = [r, g, b].map(v =>  v < 0 ? v = 0 : v).map(v => v > 255 ? v = 255 : v);
+  return rgbSanitazed.map(v =>  v.toString(16)).map(vs => Number(vs) < 10  ? '0'+vs : vs).join('').toUpperCase();  
+}
+
+// console.log('rgb(255, 255, 255)', rgb(255, 255, 255)) // returns FFFFFF
+// console.log('rgb(0,0,0)', rgb(0,0,0)) // returns 000000
+// console.log('rgb(148, 0, 211)', rgb(148, 0, 211)) // returns 9400D3
+
+/****************************************/
+
+
